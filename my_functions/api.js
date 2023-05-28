04f78ead14b90bde3f4f7bd3088d76b9ec1a2e8a
sa-lda <85459043+sa-lda@users.noreply.github.com>
2023-05-29 00:09:43 +0100

    Update api.js

--- my_functions/api.js
+++ my_functions/api.js
@@ -17,56 +17,65 @@ exports.handler = async function (event, context) {
         }
     });
 
+    let path = event.path.split("/").pop();
 
-    let name = event.path.split("/").pop()
-    if (name === "contact") {
-        return new Promise((resolve, reject) => {
-            let select = 'SELECT * FROM contact_form LIMIT 30';
-
-            db.query(select, function (err, results, fields) {
-                if (err) {
-                    console.log(err.message);
-                }
-
-                resolve({
-                    statusCode: 200,
-                    headers: {
-                        'Access-Control-Allow-Origin': '*',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(results)
+    switch (path) {
+        case "contact":
+            return new Promise((resolve, reject) => {
+                let select = 'SELECT * FROM contact_form LIMIT 30';
+    
+                db.query(select, function (err, results, fields) {
+                    if (err) {
+                        console.log(err.message);
+                    }
+    
+                    resolve({
+                        statusCode: 200,
+                        headers: {
+                            'Access-Control-Allow-Origin': '*',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(results)
+                    })
+                });
+    
+                db.end(function (err) {
+                    if (err) {
+                        return console.log(err.message);
+                    }
+                });
+            });
+            break;
+        case "barksalot":
+            return {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name: "Barksalot", species: "dog", "photo": "https://learnwebcode.github.io/json-example/images/dog-1.jpg", bio: "This dog is very communicative. Deleniti, tempora quis commodi qui inventore ratione rem porro doloribus et obcaecati cumque quibusdam voluptatibus iure nisi aut minima consequuntur, officiis esse? Lorem ipsum, dolor sit amet consectetur adipisicing elit." })
+            }
+            break;
+        case "purrsloud":
+            return {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name: "Purrsloud", species: "cat", "photo": "https://learnwebcode.github.io/json-example/images/cat-2.jpg", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores, sunt consectetur a amet dolorem rem animi tempore molestias nesciunt fuga, sequi alias voluptatum totam reprehenderit assumenda deleniti distinctio? Cumque. Lorem ipsum." })
+            }
+            break;
+        default:
+            return {
+                statusCode: 404,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    message: "Not found"
                 })
-            });
-
-            db.end(function (err) {
-                if (err) {
-                    return console.log(err.message);
-                }
-            });
-        });
-    } else if (name === "barksalot") {
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name: "Barksalot", species: "dog", "photo": "https://learnwebcode.github.io/json-example/images/dog-1.jpg", bio: "This dog is very communicative. Deleniti, tempora quis commodi qui inventore ratione rem porro doloribus et obcaecati cumque quibusdam voluptatibus iure nisi aut minima consequuntur, officiis esse? Lorem ipsum, dolor sit amet consectetur adipisicing elit." })
-        }
-    } else if (name === "purrsloud") {
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name: "Purrsloud", species: "cat", "photo": "https://learnwebcode.github.io/json-example/images/cat-2.jpg", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis asperiores, sunt consectetur a amet dolorem rem animi tempore molestias nesciunt fuga, sequi alias voluptatum totam reprehenderit assumenda deleniti distinctio? Cumque. Lorem ipsum." })
-        }
-    } else {
-        return {
-            statusCode: 404,
-            headers: { "Cache-Control": "max-age=10" },
-            body: JSON.stringify({ message: "Animal not found." })
-        }
+            }
     }
 }
