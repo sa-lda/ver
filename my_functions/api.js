530415bb7432fdce418e02f484dff4b237fd3ede
sa-lda <85459043+sa-lda@users.noreply.github.com>
2023-05-31 23:09:07 +0100

    Update api.js

--- my_functions/api.js
+++ my_functions/api.js
@@ -45,8 +45,7 @@ exports.handler = async function (event, context) {
                     resolve({
                         statusCode: 200,
                         headers: {
-                            'Access-Control-Allow-Origin': '*',
-                            'Content-Type': 'application/json'
+                            'Access-Control-Allow-Origin': '*'
                         },
                         body: JSON.stringify(results)
                     })
