a2f3cd267e026f724b6b3abb5b6dc36293cd4c16
sa-lda <85459043+sa-lda@users.noreply.github.com>
2023-07-18 19:33:50 +0100

    Update App.js

--- frontend-src/App.js
+++ frontend-src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 function App() {
-  return <h1>Forbidden</h1>;
+  return <h1>Coming Soon</h1>;
 }
 
 ReactDOM.render(<App />, document.getElementById("app"))
